6dd05dbfbb18bcc4e1b056a2b00d2a9c220bc780
Dhanapal <147046878+dhanapalmayil@users.noreply.github.com>
2024-08-25 15:34:21 +0530

    Day 5 Task

---
+++ Daily Task/Day 5 - Task -23_08_2024/Serialization/Serialization/Course.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialization
+{
+    internal class Course
+    {
+        public int Cid {  get; set; }
+        public string Cname { get; set; }
+
+        public int Cduration {  get; set; }
+
+        public Course(int Cid,string cname,int Cduration) { 
+            this.Cid = Cid;
+            this.Cname=cname;
+            this.Cduration = Cduration;
+        }
+    }
+}
