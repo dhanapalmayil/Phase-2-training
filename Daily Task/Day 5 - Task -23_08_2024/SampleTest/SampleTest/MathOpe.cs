542bad99d62ba4bbc2dfe25a6f1c680bd66ca1c8
Dhanapal <147046878+dhanapalmayil@users.noreply.github.com>
2024-08-25 15:35:56 +0530

    Add files via upload

---
+++ Daily Task/Day 5 - Task -23_08_2024/SampleTest/SampleTest/MathOpe.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleTest
+{
+    public class MathOpe
+    {
+        public int add(int a, int b)
+        {
+            return a + b;
+        }
+        public int Sub(int x, int y)
+        {
+            return x - y;
+        }
+        public int Pro(int x, int y)
+        {
+            return x * y;
+        }
+        public int Div(int x, int y)
+        {
+            return x / y;
+        }
+        public virtual bool CheckValues()
+        {
+            return false;
+        }
+    }
+}
