5d5142f7b0d4661551c64ecb8e647731dcac7373
Dhanapal <147046878+dhanapalmayil@users.noreply.github.com>
2024-09-04 09:50:38 +0530

    Add files via upload

---
+++ Daily Task/Day 12 - Task -03-09-2024/Identitytype/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+using Identitytype.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Identitytype.Controllers
+{
+    [Authorize]
+    
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("dhanapal")]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Daily Task/Day 12 - Task -03-09-2024/Identitytype/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Identitytype.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
